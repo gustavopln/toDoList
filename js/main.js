ea670177140e47a50df37f79596844453216fd41
gustavopln <gustavopln@gmail.com>
2022-11-10 23:42:34 -0300

    Adicionar e remover tarefas
    Adicionar e remover tarefas

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
-//Objeto principal da aplicação
+/*
+* Objeto principal da aplicação
+*/
 const Main = {
 
-    //Propriedade init e o valor dela é uma função
+    /*
+    * Propriedade init e o valor dela é uma função
+    */
     init: function() {
         this.cacheSelectors()
         this.bindEvents()
@@ -10,19 +14,33 @@ const Main = {
     //Responsável somente por selecionar os elementos
     cacheSelectors: function() {
         this.$checkButtons = document.querySelectorAll('.check')
+        this.$inputTask = document.querySelector('#inputTask')
+        this.$list = document.querySelector('#list')
+        this.$removeButtons = document.querySelectorAll('.remove')
     },
 
-    //Responsável por adicionar os eventos
+    /*
+    * Responsável por adicionar os eventos
+    */
     bindEvents: function() {
         const self = this
 
         this.$checkButtons.forEach(function(button){
-            //Aqui o this pega outro contexto, pega o window
+            //Aqui o this pega outro contexto, pega o window, por isso deve pegar o this através da const self para pegar no contexto geral do main
             button.onclick = self.Events.checkButton_click
+        })
+
+        //bind = ligar, passar o this do objeto para o evento
+        this.$inputTask.onkeypress = self.Events.inputTask_keypress.bind(this)
+
+        this.$removeButtons.forEach(function(button){
+            button.onclick = self.Events.removeButton_click
         })
     },
 
-    //Propriedades do nosso objeto
+    /*
+    * Propriedades do nosso objeto
+    */
     Events: {
         checkButton_click: function(e) {
             const li = e.target.parentElement
@@ -34,8 +52,42 @@ const Main = {
                 return li.classList.add('done')
             }
             
-            li.classList.remove('done')
-            
+            li.classList.remove('done')            
+        },
+
+        //dentro de uma função de evento o this sempre vai ser o elemento e não o objeto
+        inputTask_keypress: function(e){
+            const key = e.key
+            const value = e.target.value
+
+            if (key === 'Enter'){
+                this.$list.innerHTML += `
+                    <li>
+                        <div class="check"></div>
+                        <label class="task">
+                            ${value}
+                        </label>
+                        <button class="remove"></button>
+                    </li>
+                `
+
+                e.target.value = ''
+
+                //para adicionar novamente os eventos
+                this.cacheSelectors()
+                this.bindEvents()
+            }
+        },
+
+        removeButton_click: function(e){
+            //pegar o elemento pai do button que no caso é a li
+            const li = e.target.parentElement
+
+            li.classList.add('removed')
+
+            setTimeout(function(){
+                li.classList.add('hidden')
+            },300)
         }
     }
 }
